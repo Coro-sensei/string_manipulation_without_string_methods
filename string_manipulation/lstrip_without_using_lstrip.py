093cd1651540740e25c32e840c9dce32aee84a64
Coro-sensei <acoro1719@gmail.com>
2025-03-27 04:26:23 +0800

    modified:   added input and the pseudo lstrip

--- string_manipulation/lstrip_without_using_lstrip.py
+++ string_manipulation/lstrip_without_using_lstrip.py
@@ -1,5 +1,13 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
+# Initiate the  lstrip
+pseudo_lstrip = 0
+
 # Input the characters 
-# Imitate lstrip 
+character_input = input("Enter your sentence:")
+
+# Imitate lstrip
+while pseudo_lstrip < len(character_input) and s[pseudo_lstrip] == " ":
+    pseudo_lstrip += 1
+    
 # Print the result
