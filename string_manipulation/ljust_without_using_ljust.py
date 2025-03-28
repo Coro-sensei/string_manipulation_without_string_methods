ac3d2c9ca48bb91506e1b65814edb9da7acb0aad
Coro-sensei <acoro1719@gmail.com>
2025-03-28 19:44:06 +0800

    modified: imitation of ljust asterisk for visual clarity

--- string_manipulation/ljust_without_using_ljust.py
+++ string_manipulation/ljust_without_using_ljust.py
@@ -1,5 +1,12 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # Input the character
+character = input("Enter your sentence: ")
+width = 100
+
 # Imitate ljust
+if len(character) < width:
+    character += '*' * (width - len(character))
+
 # Print result
+print (f'{character}')
