c95b90aaa2e21b8403de47725d8fd417c5bea32a
Coro-sensei <acoro1719@gmail.com>
2025-03-28 20:16:37 +0800

    modified:   pseudo swapcase, using isupper, upper, and lower

--- string_manipulation/swapcase_without_using_swapcase.py
+++ string_manipulation/swapcase_without_using_swapcase.py
@@ -1,1 +1,10 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# Input character
+character = input("Enter your sentence: ")
+
+# Imitate swapcase
+pseudo_swapcase = "".join(char.lower() if char.isupper() else char.upper() for char in character)
+
+# Print result
+print(pseudo_swapcase)
