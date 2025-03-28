6c4014d3f6b80d144e0a1c6f94e1ac6eaf73b088
Coro-sensei <acoro1719@gmail.com>
2025-03-28 18:54:50 +0800

    modified:   test modified:   isupper imitation

--- string_manipulation/isupper_without_using_isupper.py
+++ string_manipulation/isupper_without_using_isupper.py
@@ -1,5 +1,9 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 
 # Input characters
+character = input("Enter your sentence: ")
+
 # Imitate isupper
+pseudo_isupper = all('A' <= char <= 'Z' for char in character if char.isalpha())
+
 # Print result
