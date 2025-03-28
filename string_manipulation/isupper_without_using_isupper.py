f4e050a2f1aca483958dfd4f5db8faae70f0db1f
Coro-sensei <acoro1719@gmail.com>
2025-03-28 18:51:26 +0800

    modified:   pseudocode

--- string_manipulation/isupper_without_using_isupper.py
+++ string_manipulation/isupper_without_using_isupper.py
@@ -1,1 +1,5 @@
 # Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+# Input characters
+# Imitate isupper
+# Print result
