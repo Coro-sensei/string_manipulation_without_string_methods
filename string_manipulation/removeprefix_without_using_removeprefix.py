c71f4067fca0143daff54dfde4a0fc334028ee71
Coro-sensei <acoro1719@gmail.com>
2025-03-28 18:28:48 +0800

    modified:   input characters, using replace and startswith

--- string_manipulation/removeprefix_without_using_removeprefix.py
+++ string_manipulation/removeprefix_without_using_removeprefix.py
@@ -1,5 +1,12 @@
 # Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 
 # Input characters
+character = input("Enter your word with a prefix: ")
+prefix_cutter = input("Enter the prefix that you want to cut: ")
+
 # Imitate removeprefix
+if character.startswith(prefix_cutter):
+    character = character.replace(prefix_cutter , "")
+
 # Print result
+print(character)
