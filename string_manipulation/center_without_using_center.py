055c95a54bf7f9e03191110248c003f51b6a9772
Coro-sensei <acoro1719@gmail.com>
2025-03-28 19:48:07 +0800

    modified:   pseudocode

--- string_manipulation/center_without_using_center.py
+++ string_manipulation/center_without_using_center.py
@@ -1,1 +1,5 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# Input character 
+# Imitate center
+# Print result
