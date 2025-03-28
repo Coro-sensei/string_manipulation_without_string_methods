6d35f5aeb5d0afe5c08047ce845437390059ea43
Coro-sensei <acoro1719@gmail.com>
2025-03-28 18:43:11 +0800

    modified:   test for imitation lower

--- string_manipulation/lower_without_using_lower.py
+++ string_manipulation/lower_without_using_lower.py
@@ -1,1 +1,9 @@
 # Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# Input characters
+character = input("Enter your sentence to be uncapitalized: ")
+
+# Imitate lower
+pseudo_lower = [chr(ord(char) + 32) if 'A' <= char <= 'Z' else char for char in character]
+
+# Print result
