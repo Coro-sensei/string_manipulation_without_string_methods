dc72e0db45dedc816167b2300c47f73c7880ce19
Coro-sensei <acoro1719@gmail.com>
2025-03-28 20:22:36 +0800

    modified:   using lower and upper to capitalized first letter of string

--- string_manipulation/capitalize_without_using_capitalize.py
+++ string_manipulation/capitalize_without_using_capitalize.py
@@ -1,5 +1,10 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 # Input character
+character = input("Enter your sentence: ")
+
 # Imitate capitalize
+if character: 
+    pseudo_capitalize = character[0].upper() + character[1:].lower()
+    
 # Print result 
