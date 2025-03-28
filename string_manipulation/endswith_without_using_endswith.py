3823d123ea4946f0009f243807fd9531a5fbb3a2
Coro-sensei <acoro1719@gmail.com>
2025-03-28 19:18:04 +0800

    modified:   used -len to check the last characters

--- string_manipulation/endswith_without_using_endswith.py
+++ string_manipulation/endswith_without_using_endswith.py
@@ -1,1 +1,10 @@
 # Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# Input character
+character = input("Enter your sentence: ")
+endingwith = input("Enter the character the sentence should end with: ")
+
+# Imitate endswith
+pseudo_endswith = character[-len(endingwith)] == endingwith
+
+# Print result
