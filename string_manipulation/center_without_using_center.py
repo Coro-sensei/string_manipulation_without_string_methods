33f982723d1d2573cbaa9114ccb345bb0e80e4f3
Coro-sensei <acoro1719@gmail.com>
2025-03-28 19:50:53 +0800

    modified:   center the sentence

--- string_manipulation/center_without_using_center.py
+++ string_manipulation/center_without_using_center.py
@@ -1,5 +1,12 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 
 # Input character 
+character = input("Enter your sentence: ")
+width = 100
+
 # Imitate center
+center = width - len(character)
+ljust = center // 2
+rjust = center - ljust
+
 # Print result
