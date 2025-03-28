dc0917f617bd2df5855bf641586c3e2cfb8e0d3b
Coro-sensei <acoro1719@gmail.com>
2025-03-28 20:25:53 +0800

    modified:   print the result and check if the character is already capitalized

--- string_manipulation/capitalize_without_using_capitalize.py
+++ string_manipulation/capitalize_without_using_capitalize.py
@@ -1,10 +1,13 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 
 # Input character
-character = input("Enter your sentence: ")
+character = str(input("Enter your sentence: "))
 
 # Imitate capitalize
 if character: 
     pseudo_capitalize = character[0].upper() + character[1:].lower()
-    
+else:
+    pseudo_capitalize = ""
+
 # Print result 
+print(pseudo_capitalize)
