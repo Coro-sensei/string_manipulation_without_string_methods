72bf60a5d72e8d93fb4fbed2f32cf5921da299c9
Coro-sensei <acoro1719@gmail.com>
2025-03-28 20:37:42 +0800

    modified:   pseudo title using upper, lower and join to copy title func

--- string_manipulation/title_without_using_title.py
+++ string_manipulation/title_without_using_title.py
@@ -1,5 +1,10 @@
 # Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 
 # Input character
+character = input("Enter your sentence: ")
+
 # Imitate title
+pseudo_title = " ".join(word[0].upper() + word[1: ].lower() if word else "" for word in character.split())
+
 # Print result
+print(pseudo_title)
