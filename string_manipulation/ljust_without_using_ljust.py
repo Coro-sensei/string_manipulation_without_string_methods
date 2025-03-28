fff60ec2628ddd1c8c29382f8aa40f11353071ea
Coro-sensei <acoro1719@gmail.com>
2025-03-28 19:37:34 +0800

    modified:   pseudocode

--- string_manipulation/ljust_without_using_ljust.py
+++ string_manipulation/ljust_without_using_ljust.py
@@ -1,1 +1,5 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# Input the character
+# Imitate ljust
+# Print result
