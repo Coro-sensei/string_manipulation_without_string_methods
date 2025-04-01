8e31f8e7e2338310994e194db413bfaa9a2c9df1
Coro-sensei <acoro1719@gmail.com>
2025-04-01 16:44:42 +0800

    modified:   islower imitation using isalpha to check

--- 2_string_manipulation/islower_without_using_islower.py
+++ 2_string_manipulation/islower_without_using_islower.py
@@ -1,1 +1,9 @@
 # Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+# Input character
+character = input("ENter your sentence: ")
+
+# Imitate  islower
+pseudo_islower = all('a' <= char <= 'z' for char in character if char.isalpha())
+
+# Print result
