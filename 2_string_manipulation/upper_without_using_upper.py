2b5eb3dbaba1e233c43a25853ee0a7f4e3d11c56
Coro-sensei <acoro1719@gmail.com>
2025-04-01 16:40:20 +0800

    modified:   test code

--- 2_string_manipulation/upper_without_using_upper.py
+++ 2_string_manipulation/upper_without_using_upper.py
@@ -1,5 +1,10 @@
 # Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 
 # Input character 
+character = input("Enter your sentence: ")
+
 # Imitate upper
+pseudo_upper = ''.join(chr(ord(char) - 32) if 'A' <= char <= 'Z' else char for char in character)
+
 # Print the result
+print(pseudo_upper)
