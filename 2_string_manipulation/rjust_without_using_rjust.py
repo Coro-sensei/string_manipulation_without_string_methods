09c3d42a4999df21b3744c95ec603fb9e04f8d47
Coro-sensei <acoro1719@gmail.com>
2025-04-01 17:01:23 +0800

    modified:   used len and list to print

--- 2_string_manipulation/rjust_without_using_rjust.py
+++ 2_string_manipulation/rjust_without_using_rjust.py
@@ -1,11 +1,12 @@
 # Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 
 # Input character
-character = input("Enter your sentence: ")
+character = str(input("Enter your sentence: "))
 width = 100
 
 # Imitate rjust
 if len(character) < width:
-    character += '*' * (width - len(character))
+    character = '*' * (width - len(character)) + character
 
 # Print result
+print(f'{character}')
