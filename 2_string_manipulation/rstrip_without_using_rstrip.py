3cba096ef5fe40dcb644ba2cab05dea14af0eb60
Coro-sensei <acoro1719@gmail.com>
2025-04-01 10:21:09 +0800

    modified:   2_string_manipulation/count_without_using_count.py 	modified:   2_string_manipulation/index_without_using_index.py 	modified:   2_string_manipulation/islower_without_using_islower.py 	modified:   2_string_manipulation/removesuffix_without_using_removesuffix.py 	modified:   2_string_manipulation/rindex_without_using_rindex.py 	modified:   2_string_manipulation/rjust_without_using_rjust.py 	modified:   pseudocode 	modified:   2_string_manipulation/startswith_without_using_startswith.py 	modified:   2_string_manipulation/upper_without_using_upper.py 	modified:   2_string_manipulation/zfill_without_using_zfill.py

---
+++ 2_string_manipulation/rstrip_without_using_rstrip.py
@@ -0,0 +1,5 @@
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+
+# Input characters
+# Imitate rstrip
+# Print result
