ab39ef90fcf2c8feea69cdebfcb5e55da880da30
Coro-sensei <acoro1719@gmail.com>
2025-04-01 17:33:44 +0800

    modified:   test code

--- 2_string_manipulation/zfill_without_using_zfill.py
+++ 2_string_manipulation/zfill_without_using_zfill.py
@@ -1,5 +1,11 @@
 # Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 
 # Input character
+character = int(input("Enter your number: "))
+width_of_zero = int(input("Enter your desired amount of zero to fill: "))
+
 # Imitate zfill
+pseudo_zfill = "0" * (width_of_zero - len(character)) + character if len(character) < width_of_zero else character
+
 # Print result
+print(pseudo_zfill)
