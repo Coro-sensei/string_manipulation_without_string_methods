530ebfbb912a1bec9ab28a0691d0f7fbbad2f01c
Coro-sensei <acoro1719@gmail.com>
2025-04-01 17:49:39 +0800

    modified:  test run

--- 2_string_manipulation/count_without_using_count.py
+++ 2_string_manipulation/count_without_using_count.py
@@ -1,5 +1,13 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 
 # Input character
+sentence = input("Enter your sentence: ")
+character_count = input("Enter what word to count : ")
+
 # Imitate count
+pseudo_counter = 0
+for char in sentence:
+    if char == character_count:
+        pseudo_counter += 1
+        
 # Print result
