f03f6c6d53b1b108313c34f1de29289c8c1e2f2c
Coro-sensei <acoro1719@gmail.com>
2025-04-01 10:40:20 +0800

    modified:   made with endswith, using replace to delete suffix

--- 2_string_manipulation/removesuffix_without_using_removesuffix.py
+++ 2_string_manipulation/removesuffix_without_using_removesuffix.py
@@ -1,5 +1,12 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 
 # Input character
+character = input("Enter your sentence: ")
+suffix_cutter = input("Enter the prefix that you want to cut: ")
+
 # Imitate removesuffix
+if character.endswith(suffix_cutter):
+    character = character.replace(suffix_cutter, "")
+
 # Print result
+print(character)
