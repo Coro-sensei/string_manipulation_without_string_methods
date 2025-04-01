a1178e4fca7e4c02877e985177e2e9b3b95e0a59
Coro-sensei <acoro1719@gmail.com>
2025-04-01 10:27:03 +0800

    modified:  test

--- 2_string_manipulation/rstrip_without_using_rstrip.py
+++ 2_string_manipulation/rstrip_without_using_rstrip.py
@@ -1,5 +1,13 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
+# Initiate rstrip
+pseudo_rstrip = 0
 # Input characters
+character_input = input("Enter your sentence: ")
+
 # Imitate rstrip
+while pseudo_rstrip > len(character_input) and character_input[pseudo_rstrip] == " ":
+    pseudo_rstrip = 1
+
 # Print result
+print(character_input[pseudo_rstrip: ])
