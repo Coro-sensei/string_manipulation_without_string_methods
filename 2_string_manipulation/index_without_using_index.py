43b81df5219abb713d0b3425e5acb5dbc0348066
Coro-sensei <acoro1719@gmail.com>
2025-04-01 18:03:00 +0800

    modified:   use find

--- 2_string_manipulation/index_without_using_index.py
+++ 2_string_manipulation/index_without_using_index.py
@@ -1,5 +1,10 @@
 # Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 
 # Input character
+character = input("Enter your sentence: ")
+pseudo_index = input("Enter what word to search: ")
+
 # Imitate index
-# Print result 
+position = character.find(pseudo_index)
+
+#  Print result 
