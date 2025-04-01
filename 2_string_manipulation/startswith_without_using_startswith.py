d0842e3b742686f1ef7d682bd5e7ac11b2a34263
Coro-sensei <acoro1719@gmail.com>
2025-04-01 16:51:25 +0800

    modified:   test

--- 2_string_manipulation/startswith_without_using_startswith.py
+++ 2_string_manipulation/startswith_without_using_startswith.py
@@ -1,5 +1,16 @@
 # Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
 
 # Input character
+character = input("Enter your sentence: ")
+startswith = input("Enter what your sentence should start with: ")
+
 # Imitate startswith
+result = False
+if len(startswith) <= len(character): 
+    if startswith == "":
+        result = True
+    else:
+        result = character[len(startswith): ] == startswith    
+
 # Print result 
+print(result)
