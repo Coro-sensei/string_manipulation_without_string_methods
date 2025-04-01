c8d4de6de83a57df58c0d002f69db4719ecf6c21
Coro-sensei <acoro1719@gmail.com>
2025-04-01 17:38:07 +0800

    modified:  pseudocode

--- 2_string_manipulation/count_without_using_count.py
+++ 2_string_manipulation/count_without_using_count.py
@@ -1,1 +1,5 @@
 # Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# Input character
+# Imitate count
+# Print result
